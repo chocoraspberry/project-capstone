f68207cfd3a3d8bee172c6ea3fbf9692dfcf6590
sliang <suweiliang666@gmail.com>
2022-03-23 12:44:51 -0700

    second try

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
@@ -11,4 +12,5 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
+
     manager.run()
